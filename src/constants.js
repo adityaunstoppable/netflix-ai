3ef3ad1576c21b786dd40ad6e2ee63430e2151a0
REELUP <reelup@REELUPs-MacBook-Pro.local>
2024-08-05 20:16:16 +0530

    Initial commit

--- src/constants.js
+++ src/constants.js
@@ -1,75 +1,75 @@
-export const MAIN_LOGO = "/img/main_live_stream_logo.png";
+// export const MAIN_LOGO = "/img/main_live_stream_logo.png";
 
-export const SIGNUP_BG = "https://assets.nflxext.com/ffe/siteui/vlv3/335ddde7-3955-499c-b4cc-ca2eb7e1ae71/a7d20bc1-831c-4f9d-8153-11bdf7a08d23/IN-en-20240624-POP_SIGNUP_TWO_WEEKS-perspective_WEB_13cda806-d858-493e-b4aa-f2792ff965dc_medium.jpg";
+// export const SIGNUP_BG = "https://assets.nflxext.com/ffe/siteui/vlv3/335ddde7-3955-499c-b4cc-ca2eb7e1ae71/a7d20bc1-831c-4f9d-8153-11bdf7a08d23/IN-en-20240624-POP_SIGNUP_TWO_WEEKS-perspective_WEB_13cda806-d858-493e-b4aa-f2792ff965dc_medium.jpg";
 
-export const ASK_AI_BG = "/img/ai_image.jpg"
+// export const ASK_AI_BG = "/img/ai_image.jpg"
 
-export const SIGN_UP = "Sign Up";
-export const SIGN_IN = "Sign In";
+// export const SIGN_UP = "Sign Up";
+// export const SIGN_IN = "Sign In";
 
-export const MOVIE_GENRES = [
-  {
-    genreKey:'',
-    genreName:'Popular Movies'  
-  },
-  {
-    genreKey:35,
-    genreName:'Comedy Movies'  
-  },
-  {
-    genreKey:878,
-    genreName:'Sci-fi And Mysteries'  
-  },
-  {
-    genreKey:27,
-    genreName:'Horror Movies'  
-  },
-  {
-    genreKey:10749,
-    genreName:'Romantic Vibes'  
-  },
-  {
-    genreKey:53,
-    genreName:'Action And Thriller'  
-  },
-]
+// export const MOVIE_GENRES = [
+//   {
+//     genreKey:'',
+//     genreName:'Popular Movies'  
+//   },
+//   {
+//     genreKey:35,
+//     genreName:'Comedy Movies'  
+//   },
+//   {
+//     genreKey:878,
+//     genreName:'Sci-fi And Mysteries'  
+//   },
+//   {
+//     genreKey:27,
+//     genreName:'Horror Movies'  
+//   },
+//   {
+//     genreKey:10749,
+//     genreName:'Romantic Vibes'  
+//   },
+//   {
+//     genreKey:53,
+//     genreName:'Action And Thriller'  
+//   },
+// ]
 
-export const TMDB_OPTIONS = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      `Bearer ${process.env.REACT_APP_AUTH_TOKEN_TMDB}`,
-  },
-};
+// export const TMDB_OPTIONS = {
+//   method: "GET",
+//   headers: {
+//     accept: "application/json",
+//     Authorization:
+//       `Bearer ${process.env.REACT_APP_AUTH_TOKEN_TMDB}`,
+//   },
+// };
 
-export const TMDB_POPULAR_MOVIES_API = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
-export const TMDB_GENRES_MOVIE_LIST_API =  `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=`
-export const TMDB_MOVIE_POSTER_API = "https://image.tmdb.org/t/p/w300"
-export const TMDB_MOVIE_INFO_API = "https://api.themoviedb.org/3/movie/"
-export const TMDB_MOVIE_SEARCH_API = "https://api.themoviedb.org/3/search/movie?&language=en-US&sort_by=popularity.desc&query="
+// export const TMDB_POPULAR_MOVIES_API = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
+// export const TMDB_GENRES_MOVIE_LIST_API =  `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=`
+// export const TMDB_MOVIE_POSTER_API = "https://image.tmdb.org/t/p/w300"
+// export const TMDB_MOVIE_INFO_API = "https://api.themoviedb.org/3/movie/"
+// export const TMDB_MOVIE_SEARCH_API = "https://api.themoviedb.org/3/search/movie?&language=en-US&sort_by=popularity.desc&query="
 
-export const YOUTUBE_URL = "https://www.youtube.com/embed/"
+// export const YOUTUBE_URL = "https://www.youtube.com/embed/"
 
-export const ASK_AI_PATH = 'ask-ai'
-export const BROWSE_PATH = '/browse'
-export const SIGNUP_PATH = '/'
+// export const ASK_AI_PATH = 'ask-ai'
+// export const BROWSE_PATH = '/browse'
+// export const SIGNUP_PATH = '/'
 
-export const OPEN_AI_API_KEY = process.env.REACT_APP_OPEN_AI_API_KEY_SECRET
-export const OPEN_AI_BASE_URL = 'https://api.openai.com/v1'
+// export const OPEN_AI_API_KEY = process.env.REACT_APP_OPEN_AI_API_KEY_SECRET
+// export const OPEN_AI_BASE_URL = 'https://api.openai.com/v1'
 
-export const GEMINI_AI_API_KEY = process.env.REACT_APP_GEMINI_SECRET_KEY
-export const GEMINI_AI_BASE_URL = 'https://api.google.dev/gemini/v1'
+// export const GEMINI_AI_API_KEY = process.env.REACT_APP_GEMINI_SECRET_KEY
+// export const GEMINI_AI_BASE_URL = 'https://api.google.dev/gemini/v1'
 
-export const QUERY_RESULTS_HELPER = {
-  loading:'Thankyou, we are working on getting best results based on your query.',
-  onResult:'Here are some results based on your query.'
-}
+// export const QUERY_RESULTS_HELPER = {
+//   loading:'Thankyou, we are working on getting best results based on your query.',
+//   onResult:'Here are some results based on your query.'
+// }
 
-export const DUMMY_MOVIE_SUGGESTIONS = [
-  "Chandu Champion",
-  "Agneepath",
-  "Dhoom 2",
-  "Inside Out 2",
-  "Krrish",
-]
+// export const DUMMY_MOVIE_SUGGESTIONS = [
+//   "Chandu Champion",
+//   "Agneepath",
+//   "Dhoom 2",
+//   "Inside Out 2",
+//   "Krrish",
+// ]
